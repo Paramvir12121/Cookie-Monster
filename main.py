195dae5f159adb970e2e52f76b1d6dd6d5a2ffbc
paramvir12121 <paramvir12121@gmail.com>
2024-02-20 12:39:30 -0500

    minor updates :

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By 
 
 # To keep webdriver running
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://amazon.ca")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-
+# cookies_number = driver.find_element(By.ID, value="cookies").text
+# print(cookies_number)
 
 # driver.close()  # Closes the page
 # driver.quit()   # Quits the browser

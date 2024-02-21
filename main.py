16d2d14b617ddbdb25fd55a5ed46e24b6464c71d
Paramvir Singh <paramvir12121@gmail.com>
2024-02-21 07:39:08 +0000

    clicking test code written, testing not done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.by import By
+import time
 
 # To keep webdriver running
 chrome_options = webdriver.ChromeOptions()
@@ -8,7 +9,20 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-# cookies_number = driver.find_element(By.ID, value="cookies").text
+language_button = driver.find_element(By.ID, value="langSelect-EN")
+language_button.click()
+time.sleep(5)
+
+
+close_cookies_btn = driver.find_element(By.XPATH,"//a[@data-cc-event='click:dismiss']")
+close_cookies_btn.click()
+while True:
+    big_cookie = driver.find_element(By.ID, "bigCookie")
+    big_cookie.click()
+    
+
+
+
 # print(cookies_number)
 
 # driver.close()  # Closes the page

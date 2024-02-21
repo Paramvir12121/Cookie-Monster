5dfa2c3714420386c967609b1e542e9db3058ffa
paramvir12121 <paramvir12121@gmail.com>
2024-02-21 13:35:16 -0500

    basic working bot

--- main.py
+++ main.py
@@ -8,17 +8,24 @@ def big_cookie_thread():
     while True:
         big_cookie = driver.find_element(By.ID, "bigCookie")
         big_cookie.click()
-        time.sleep(1)
 
 # Thread function to click on upgrades
 def click_upgrades():
     while True:
-        upgrade = driver.find_element(By.XPATH,'//*[@id="upgrades unlocked enabled"]')
-        if upgrade:
-            upgrade[-1].click()
-        products = driver.find_element(By.XPATH,'//*[@class="product unlocked enabled"]')
-        if products:
-            products[-1].click()
+        try:
+            upgrade = driver.find_elements(By.XPATH,'//div[@class="upgrades unlocked enabled"]')
+            if upgrade:
+                upgrade[-1].click()
+        except:
+            pass
+        try:
+            products = driver.find_elements(By.XPATH,'//div[@class="product unlocked enabled"]')
+            print("product : ",products)
+            print("Product Type: ", type(products))
+            if products:
+                products[-1].click()
+        except:
+            pass
         time.sleep(5)
 
 # To keep webdriver running
@@ -29,12 +36,11 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.maximize_window()
 
-time.sleep(5)
+time.sleep(3)
 language_button = driver.find_element(By.ID, value="langSelect-EN")
-print(language_button)
 language_button.click()
 
-time.sleep(5)
+time.sleep(3)
 close_cookies_btn = driver.find_element(By.XPATH,"/html/body/div[1]/div/a[1]")
 close_cookies_btn.click()
 
@@ -44,6 +50,7 @@ cookie_click_t = threading.Thread(target=big_cookie_thread)
 cookie_click_t.start()
 upgrades_click_t = threading.Thread(target=click_upgrades)
 upgrades_click_t.start()
+
 
 
 
